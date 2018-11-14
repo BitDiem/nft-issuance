4925f3efbbc69d5983261d5d251c66f6a8219160
Arash Emami <aemami99@gmail.com>
2018-11-14 01:58:33 -0800

    Added optimization settings to the truffle config files

--- truffle-config.js
+++ truffle-config.js
@@ -19,7 +19,15 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*"
+      network_id: "*",
+      gasPrice: 1,
+      gas: 6000000
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 };
